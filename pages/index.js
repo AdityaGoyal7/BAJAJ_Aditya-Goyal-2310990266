11f445f999f1fce6b7be267ad6f578fcf7915dff
AdityaGoyal7 <goyaladi070605@gmail.com>
2026-06-25 00:08:21 +0530

    Bajaj Final

--- pages/index.js
+++ pages/index.js
@@ -47,7 +47,7 @@ export default function Home() {
     setError(''); setResult(null); setLoading(true);
     const data = input.split(/[\n,]+/).map(s => s.trim()).filter(Boolean);
     try {
-      const res = await fetch('/api/bfhl', {
+      const res = await fetch('/bfhl', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ data }),
